b07ff8c058d5b461d643b474cbcdbaae00ea9761
janekb04 <janekb04@icloud.com>
2021-03-27 15:43:13 +0100

    Add test provider

--- src/main.cpp
+++ src/main.cpp
@@ -5,15 +5,30 @@
 #include <chrono>
 #include <thread>
 
-int main()
+int main(int argc, char* argv[])
 {
-	server messenger{[]() {
-		return to_json({atmospheric_dust(), water_turbidity(), water_temperature(),
-		                atmospheric_humidity(), atmospheric_pressure(), atmospheric_temperature()});
-	}};
+	// for now by default use a test provider, as the real one works only on CyberDuck
+	auto provider = +[]() {
+		return to_json(
+		    {rand() / static_cast<float>(RAND_MAX), rand() / static_cast<float>(RAND_MAX),
+		     rand() / static_cast<float>(RAND_MAX), rand() / static_cast<float>(RAND_MAX),
+		     rand() / static_cast<float>(RAND_MAX), rand() / static_cast<float>(RAND_MAX)});
+	};
+
+	// use the real provider
+	if (argc == 2)
+	{
+		provider = +[]() {
+			return to_json({atmospheric_dust(), water_turbidity(), water_temperature(),
+			                atmospheric_humidity(), atmospheric_pressure(),
+			                atmospheric_temperature()});
+		};
+	}
+
+	server messenger{provider};
 	while (true)
 	{
-        using namespace std::chrono_literals;
+		using namespace std::chrono_literals;
 		std::this_thread::sleep_for(1000ms);
 	}
 }
